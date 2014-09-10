4fbd586ea6e30cfa7a01739c428a17b39be189f8
Boris Bobrov <breton@cynicmansion.ru>
2014-09-10 06:57:46 +0400

    fixes to hl

--- src/documents/utils.py
+++ src/documents/utils.py
@@ -21,7 +21,7 @@ class MultilineHighlighter(Highlighter):
         term_list = []
 
         for term, locations in highlight_locations.items():
-            term_list += [(loc - start_offset, term) for loc in locations]
+            term_list += [(loc, term) for loc in locations]
 
         loc_to_term = sorted(term_list)
 
@@ -36,7 +36,6 @@ class MultilineHighlighter(Highlighter):
         # Copy the part from the start of the string to the first match,
         # and there replace the match with a highlighted version.
         highlighted_result = ''
-        matched_so_far = 0
         padding = settings.HAYSTACK_HIGHLIGHTED_SYMBOLS
 
         for cur, cur_str in loc_to_term:
@@ -50,16 +49,9 @@ class MultilineHighlighter(Highlighter):
                 if symbols_start < 0:
                     symbols_start = 0
                 after_term_idx = cur + len(actual_term)
-                highlighted_result += ('<div>' + text[symbols_start:cur] +
+                highlighted_result += ('<div>...' + text[symbols_start:cur] +
                         hl_start + actual_term + hl_end +
                         text[after_term_idx:after_term_idx + padding] +
-                        '</div>')
-                # Keep track of how far we've copied so far, for the last step
-                matched_so_far = cur + len(actual_term)
-
-        # Don't forget the chunk after the last term
-
-        if start_offset > 0:
-            highlighted_result = '...%s...' % highlighted_result
+                        '...</div>')
 
         return highlighted_result
